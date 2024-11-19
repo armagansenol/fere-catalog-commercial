78805d71f7e4b0113e244bc28481126f27649bf5
armagansenol <armagansnl@gmail.com>
2024-11-19 15:33:49 +0300

    test

--- lib/api/index.ts
+++ lib/api/index.ts
@@ -4,6 +4,9 @@ const apiClient = axios.create({
   baseURL: "https://cms.ferecatalog.com/services",
   headers: {
     "Content-Type": "application/json",
+    "Cache-Control": "no-cache",
+    Pragma: "no-cache",
+    Expires: "0",
   },
 })
 

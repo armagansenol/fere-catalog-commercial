78242dcb4f9e313fecfb1b7d66f3374e1fd86bbe
armagansenol <armagansnl@gmail.com>
2024-11-19 09:44:11 +0300

    remove auto cache

--- services/main-slider.ts
+++ services/main-slider.ts
@@ -2,10 +2,19 @@
 
 import apiClient from "@/lib/api"
 import { MainSliderProps } from "@/types"
+import { AxiosRequestConfig } from "axios"
 
 export async function getMainSlider(lang?: string) {
   try {
-    const response = await apiClient.post<MainSliderProps[]>("/banner.php", lang ? { lang } : undefined)
+    const config: AxiosRequestConfig = {
+      headers: {
+        "Cache-Control": "no-cache",
+        Pragma: "no-cache",
+        Expires: "0",
+      },
+    }
+
+    const response = await apiClient.post<MainSliderProps[]>("/banner.php", lang ? { lang } : undefined, config)
     return response.data
   } catch (error) {
     console.error("Error fetching banners:", error)

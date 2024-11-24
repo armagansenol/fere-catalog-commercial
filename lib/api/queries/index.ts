59edef07c3e05cd56a3be9e1415323cc8d2b1e14
armagansenol <armagansnl@gmail.com>
2024-11-24 10:51:29 +0300

    test

--- lib/api/queries/index.ts
+++ lib/api/queries/index.ts
@@ -4,9 +4,7 @@ import { MainSliderProps, Testimonial } from "@/types"
 import { fetchWithErrorHandling } from ".."
 
 export async function getMainSlider(): Promise<MainSliderProps[]> {
-  return fetchWithErrorHandling<MainSliderProps[]>(`banner.php`, {
-    next: { revalidate: 0 },
-  })
+  return fetchWithErrorHandling<MainSliderProps[]>(`banner.php`)
 }
 
 export async function getTestimonials(): Promise<Testimonial[]> {

8919d4f5e212b4f8914bcafd7cc0ab052f1e89f5
armagansenol <armagansnl@gmail.com>
2024-11-24 10:29:38 +0300

    remove cache

--- lib/api/queries/index.ts
+++ lib/api/queries/index.ts
@@ -4,7 +4,9 @@ import { MainSliderProps, Testimonial } from "@/types"
 import { fetchWithErrorHandling } from ".."
 
 export async function getMainSlider(): Promise<MainSliderProps[]> {
-  return fetchWithErrorHandling<MainSliderProps[]>(`banner.php`)
+  return fetchWithErrorHandling<MainSliderProps[]>(`banner.php`, {
+    next: { revalidate: 0 },
+  })
 }
 
 export async function getTestimonials(): Promise<Testimonial[]> {

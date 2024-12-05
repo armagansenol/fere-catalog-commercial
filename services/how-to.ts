1bc2f280e287ea4c16096e8cadba875e58c38580
armagansenol <armagansnl@gmail.com>
2024-12-05 18:36:08 +0300

    update services

--- services/how-to.ts
+++ services/how-to.ts
@@ -3,21 +3,11 @@ import { HowToItem } from "@/types"
 export async function getHowTo(lang?: string): Promise<HowToItem[]> {
   const url = "https://cms.ferecatalog.com/services/howto.php"
 
-  try {
-    const response = await fetch(url, {
-      method: "POST",
-      body: lang ? JSON.stringify({ lang }) : undefined,
-      cache: "no-store",
-    })
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`)
-    }
-    const data = await response.json()
-    console.log("data", data)
-    return data as HowToItem[]
-  } catch (error) {
-    console.error("Error fetching banners:", error)
-    throw new Error("Failed to fetch banners")
-  }
+  const response = await fetch(url, {
+    method: "POST",
+    body: lang ? JSON.stringify({ lang }) : undefined,
+    cache: "no-store",
+  })
+  const data = await response.json()
+  return data as HowToItem[]
 }

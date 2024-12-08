4e097959922c9398e8223dec702c5dd0736a415b
armagansenol <armagansnl@gmail.com>
2024-12-08 18:43:58 +0300

    chore: update icon

--- components/utility/modal/Modal.tsx
+++ components/utility/modal/Modal.tsx
@@ -4,13 +4,13 @@ import s from "./modal.module.scss"
 
 import { ScrollTrigger, gsap } from "@/lib/gsap"
 import { useGSAP } from "@gsap/react"
-import { useRef } from "react"
 import cx from "clsx"
+import { useRef } from "react"
 
+import { useCursorStore } from "@/lib/store/cursor"
 import { useLenisStore } from "@/lib/store/lenis"
 import { useModalStore } from "@/lib/store/modal"
-import { useCursorStore } from "@/lib/store/cursor"
-import { Cross2Icon } from "@radix-ui/react-icons"
+import { X } from "lucide-react"
 
 const Modal = () => {
   const { isOpen, content, setIsOpen, setContent } = useModalStore()
@@ -55,7 +55,7 @@ const Modal = () => {
   return (
     <div className={cx(s.modal, "flex items-center justify-center")} ref={ref}>
       <div className={cx(s.iconClose, "cursor-pointer z-10")} onClick={closeModal}>
-        <Cross2Icon className="w-full h-full" />
+        <X className="w-full h-full" />
       </div>
       <div className="relative z-0">{content}</div>
     </div>

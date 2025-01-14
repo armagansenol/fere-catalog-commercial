d5b2ac5b2c4ba5afa3a570e58eb77c01880fddbd
armagansenol <armagansnl@gmail.com>
2025-01-14 14:40:04 +0300

    feat: add favicon images and update layout metadata for enhanced branding and user experience

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -23,6 +23,16 @@ export const metadata: Metadata = {
   title: "Fere Catalog | Online Katalog ve Sipariş Platformu",
   description:
     "Fere ile online mağazanızı kolayca oluşturun, ürünlerinizi kategorize edin ve siparişlerinizi tek bir platformdan yönetin. Hemen ücretsiz denemeye başlayın!",
+  icons: {
+    icon: [
+      { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/favicon/android-chrome-512x512.png", sizes: "512x512", type: "image/png" },
+      { url: "/favicon/favicon.ico", sizes: "any", type: "image/x-icon" },
+    ],
+    apple: [{ url: "/favicon/apple-touch-icon.png", sizes: "180x180", type: "image/png" }],
+  },
 }
 
 export default async function MainLayout({ children }: { children: React.ReactNode }) {

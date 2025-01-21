403bb5f6e363e30e435129d91d5ff676a45f80ed
armagansenol <armagansnl@gmail.com>
2025-01-21 17:41:55 +0300

    feat: integrate Google Tag Manager for enhanced tracking and analytics capabilities

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "@/styles/global.scss"
 import "@/styles/tailwind-base.css"
+import Script from "next/script"
 
 export default async function RootLayout({
   children,
@@ -8,7 +9,26 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-K9P2FSH6');`}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
+      <body className={`antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K9P2FSH6"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {children}
+      </body>
     </html>
   )
 }
